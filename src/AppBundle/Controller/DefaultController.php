b89ffe3f4fb805f5147da0b1feeeaea852715626
Evgen Salyakin <e.salyakin@gmail.com>
2015-11-24 01:25:00 +0200

    fix

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,12 +10,10 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Template
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return [];
     }
 }
