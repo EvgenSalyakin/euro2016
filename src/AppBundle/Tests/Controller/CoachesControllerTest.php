9ac4edbed1932cf39c5e1f77220c71228cdaed27
Evgen Salyakin <e.salyakin@gmail.com>
2015-11-24 14:46:32 +0200

    test add coach

--- src/AppBundle/Tests/Controller/CoachesControllerTest.php
+++ src/AppBundle/Tests/Controller/CoachesControllerTest.php
@@ -4,15 +4,15 @@ namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class CoachesControllerTest extends WebTestCase
 {
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/coach/1');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-
+        $this->assertContains('Coach', $crawler->filter('h1')->text());
     }
 }
